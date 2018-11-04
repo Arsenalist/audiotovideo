c6457e58687d29982abc3d1e511def3309f7961c
Arsenalist <zarars@gmail.com>
2018-11-04 09:18:53 -0500

    Command line parameter for background; shorter argument indicators

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ def execute():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("soundcloud_url", help="Soundcloud track URL is required")
-    parser.add_argument("--yt-cat", help="YouTube category ID", type=int, default=17)
+    parser.add_argument("-c", "--yt-cat", help="YouTube category ID", type=int, default=17)
+    parser.add_argument("-b", "--background", help="Background for video - defaults to Soundcloud")
     args = parser.parse_args()
 
     settings = settingsprovider.get_settings()
@@ -19,7 +20,10 @@ def execute():
     soundcloud_track = sc.get_soundcloud_track(soundcloud_client_id, soundcloud_track_url)
     audio_file = resourcedownloader.download_file(soundcloud_track.download_url + "?client_id=" +
                                                   soundcloud_client_id, "audio.mp3")
-    image_file = resourcedownloader.download_file(sc.get_highres_image(soundcloud_track.artwork_url), "image.jpg")
+    if args.background is None:
+        image_file = resourcedownloader.download_file(sc.get_highres_image(soundcloud_track.artwork_url), "image.jpg")
+    else:
+        image_file = resourcedownloader.download_file(args.background)
     ffmpegconverter.convert_to_video(image_file, audio_file, "video.mp4")
     yt.execute_youtube_upload("video.mp4", soundcloud_track.title, soundcloud_track.description, yt_cat_id)
 
