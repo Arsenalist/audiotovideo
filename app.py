d26a341f517ff996fd3c66949475d8c8f0c6ebae
Arsenalist <zarars@gmail.com>
2018-11-04 01:36:46 -0500

    Use SC track URL instead of id

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import argparse
 def execute():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--sc-track-id", required=True, help="Soundcloud track ID is required", type=int)
+    parser.add_argument("--sc-url", required=True, help="Soundcloud track URL is required")
     args = parser.parse_args()
 
     settings = settingsprovider.get_settings()
     soundcloud_client_id = settings['soundcloud']['client_id']
-    soundcloud_track_id = args.sc_track_id
-    soundcloud_track = sc.get_soundcloud_track(soundcloud_client_id, soundcloud_track_id)
+    soundcloud_track_url = args.sc_url
+    soundcloud_track = sc.get_soundcloud_track(soundcloud_client_id, soundcloud_track_url)
     audio_file = resourcedownloader.download_file(soundcloud_track.download_url + "?client_id=" +
                                                   soundcloud_client_id, "audio.mp3")
     image_file = resourcedownloader.download_file(sc.get_highres_image(soundcloud_track.artwork_url), "image.jpg")
